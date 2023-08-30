99bea21af067c534193ab0b85b0055e4c8c8a0f4
Osman Fitrat <osman.karamizrak@gmail.com>
2023-08-31 02:04:22 +0300

    .

--- WebApp.TemplatePattern/Models/AppUser.cs
+++ WebApp.TemplatePattern/Models/AppUser.cs
@@ -4,5 +4,8 @@ namespace WebApp.TemplatePattern.Models
 {
     public class AppUser:IdentityUser
     {
+        public string PictureUrl { get; set; }
+        public string Description { get; set; }
+        
     }
 }
---
+++ WebApp.TemplatePattern/UserCards/PrimeUserCardTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace WebApp.TemplatePattern.UserCards
+{
+    public class PrimeUserCardTemplate:UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            var sb = new StringBuilder();
+            sb.Append(@"<a href='#' class='card-link'> Mesaj Gönder</a>");
+            sb.Append(@"<a href='#' class='card-link'> Profil</a>");
+            return sb.ToString();
+        }
+
+        protected override string SetPicture()
+        {
+            return $@"<img class='card-img-top' src='{User.PictureUrl}' >";
+        }
+    }
+}
